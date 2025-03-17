477b7fb66eb0d8094b8746dd71addba388610004
MariaSurina <Maria.Surina@urfu.me>
2025-03-17 21:55:22 +0500

    подтверждение выхода

---
+++ robots/src/gui/MenuWindow.java
@@ -0,0 +1,103 @@
+package gui;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import java.awt.event.KeyEvent;
+
+import log.Logger;
+
+public class MenuWindow {
+
+    private final MainApplicationFrame mainFrame;
+
+    public MenuWindow(MainApplicationFrame mainFrame) {
+        this.mainFrame = mainFrame;
+    }
+
+    public JMenuBar generateMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        menuBar.add(createFileMenu());
+        menuBar.add(createLookAndFeelMenu());
+        menuBar.add(createTestMenu());
+
+        return menuBar;
+    }
+
+    private JMenu createFileMenu() {
+        JMenu fileMenu = new JMenu("Действия");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+
+        JMenuItem exitItem = new JMenuItem("Выход", KeyEvent.VK_X);
+        exitItem.addActionListener((event) -> confirmExit());
+        fileMenu.add(exitItem);
+
+        return fileMenu;
+    }
+
+    private JMenu createLookAndFeelMenu() {
+        JMenu lookAndFeelMenu = new JMenu("Режим отображения");
+        lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
+        lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
+                "Управление режимом отображения приложения");
+
+        JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
+        systemLookAndFeel.addActionListener((event) -> setLookAndFeel(UIManager.getSystemLookAndFeelClassName()));
+        lookAndFeelMenu.add(systemLookAndFeel);
+
+        JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
+        crossplatformLookAndFeel.addActionListener((event) -> setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName()));
+        lookAndFeelMenu.add(crossplatformLookAndFeel);
+
+        return lookAndFeelMenu;
+    }
+
+    private JMenu createTestMenu() {
+        JMenu testMenu = new JMenu("Тесты");
+        testMenu.setMnemonic(KeyEvent.VK_T);
+        testMenu.getAccessibleContext().setAccessibleDescription(
+                "Тестовые команды");
+
+        JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
+        addLogMessageItem.addActionListener((event) -> Logger.debug("Новая строка"));
+        testMenu.add(addLogMessageItem);
+
+        return testMenu;
+    }
+
+    private void setLookAndFeel(String className) {
+        try {
+            UIManager.setLookAndFeel(className);
+            SwingUtilities.updateComponentTreeUI(mainFrame);
+        } catch (ClassNotFoundException | InstantiationException
+                 | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            // just ignore
+        }
+    }
+
+    public void confirmExit() {
+        SwingUtilities.invokeLater(() -> {
+            String[] options = {"Да, мне жаль", "Нет Нет Нет!!"};
+
+            int response = JOptionPane.showOptionDialog(
+                    mainFrame,
+                    "Уже уходите???",
+                    "Подтверждение выхода",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    options,
+                    options[1]
+            );
+
+            if (response == 0) {
+                System.exit(0);
+            }
+        });
+    }
+}
