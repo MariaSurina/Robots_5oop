464777bc1393c4875cdbdc73eca104f773a83359
MariaSurina <Maria.Surina@urfu.me>
2025-04-04 22:27:24 +0500

    exit_confirm_review

--- robots/src/gui/LogWindow.java
+++ robots/src/gui/LogWindow.java
@@ -1,26 +1,20 @@
 package gui;
-
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.Rectangle;
 import java.awt.TextArea;
-import java.util.prefs.Preferences;
-
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
-
 import log.LogChangeListener;
 import log.LogEntry;
 import log.LogWindowSource;
-
-public class LogWindow extends JInternalFrame implements LogChangeListener, WindowState {
+public class LogWindow extends JInternalFrame implements LogChangeListener
+{
     private LogWindowSource m_logSource;
     private TextArea m_logContent;
-    private final Preferences prefs = Preferences.userNodeForPackage(LogWindow.class);
 
-    public LogWindow(LogWindowSource logSource) {
+    public LogWindow(LogWindowSource logSource)
+    {
         super("Протокол работы", true, true, true, true);
-        setName("LogWindow");
         m_logSource = logSource;
         m_logSource.registerListener(this);
         m_logContent = new TextArea("");
@@ -31,12 +25,12 @@ public class LogWindow extends JInternalFrame implements LogChangeListener, Wind
         getContentPane().add(panel);
         pack();
         updateLogContent();
-        load();
     }
-
-    private void updateLogContent() {
+    private void updateLogContent()
+    {
         StringBuilder content = new StringBuilder();
-        for (LogEntry entry : m_logSource.all()) {
+        for (LogEntry entry : m_logSource.all())
+        {
             content.append(entry.getMessage()).append("\n");
         }
         m_logContent.setText(content.toString());
@@ -44,25 +38,8 @@ public class LogWindow extends JInternalFrame implements LogChangeListener, Wind
     }
 
     @Override
-    public void onLogChanged() {
+    public void onLogChanged()
+    {
         EventQueue.invokeLater(this::updateLogContent);
-    }
-
-    @Override
-    public void save() {
-        Rectangle bounds = getBounds();
-        prefs.putInt("LogWindow_X", bounds.x);
-        prefs.putInt("LogWindow_Y", bounds.y);
-        prefs.putInt("LogWindow_WIDTH", bounds.width);
-        prefs.putInt("LogWindow_HEIGHT", bounds.height);
-    }
-
-    @Override
-    public void load() {
-        int x = prefs.getInt("LogWindow_X", 10);
-        int y = prefs.getInt("LogWindow_Y", 10);
-        int width = prefs.getInt("LogWindow_WIDTH", 300);
-        int height = prefs.getInt("LogWindow_HEIGHT", 800);
-        setBounds(x, y, width, height);
     }
 }
